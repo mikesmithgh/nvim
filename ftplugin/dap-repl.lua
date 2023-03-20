ea21776c99eff222b80467808bf9787c9ec0a1c3
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2023-03-20 08:00:51 -0400

    fix(dap): use "" instead of false for colorcolumn

--- ftplugin/dap-repl.lua
+++ ftplugin/dap-repl.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "gF", "<c-w>sgF", { buffer = true })
 
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
-vim.opt_local.colorcolumn = false
+vim.opt_local.colorcolumn = ""
 vim.opt_local.cursorcolumn = false
 
 require('dap.ext.autocompl').attach()
