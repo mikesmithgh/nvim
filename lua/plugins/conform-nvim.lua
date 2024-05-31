aceb45d0da79be6715432165f60a4e00b25f3ba5
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-05-31 09:31:05 -0400

    chore: conform tweaks

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -4,11 +4,13 @@ return {
   lazy = true,
   event = 'VeryLazy',
   config = function()
-    require('conform').setup({
+    local conform = require('conform')
+    conform.setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         sh = { 'shfmt' },
         go = { 'goimports', 'gofumpt' },
+        fish = { 'fish_indent' },
       },
       -- If this is set, Conform will run the formatter on save.
       -- It will pass the table to conform.format().
@@ -19,6 +21,9 @@ return {
         timeout_ms = 500,
       },
     })
+    conform.formatters.shfmt = {
+      prepend_args = { '--indent', '2' },
+    }
     -- The conform formatexpr should fall back to LSP when no formatters are
     -- available, and fall back to the internal default if no LSP clients are
     -- available. So it should be safe to set it globally.
