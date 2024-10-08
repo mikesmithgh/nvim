acf2c824aafa02f1b56fc60c3fcabb336968aa39
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-10-08 12:03:49 -0400

    chore: disable image.nvim for markdown

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,7 +1,7 @@
 return {
   '3rd/image.nvim',
   cond = vim.env.KITTY_SCROLLBACK_NVIM ~= 'true',
-  ft = { 'norg', 'markdown' },
+  ft = { 'norg' },
   lazy = true,
   enabled = true,
   config = true,
