a615d765e6a0a6fa2a30f44473f58739aa97324e
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2025-01-02 23:32:36 -0500

    chore: add note to markdown-preview-nvim

--- lua/plugins/markdown-preview-nvim.lua
+++ lua/plugins/markdown-preview-nvim.lua
@@ -8,6 +8,8 @@ return {
     'markdown',
   },
   build = function()
+    -- if you are having problems, try running $HOME/.local/share/nvim/lazy/markdown-preview.nvim/app/install.sh
+    -- see https://github.com/iamcco/markdown-preview.nvim/issues/424#issuecomment-1033083561
     vim.fn['mkdp#util#install']()
   end,
 }
