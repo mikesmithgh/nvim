a09373ca30b176a224e20ef386184ca98a79841b
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2025-02-18 20:56:27 -0500

    chore: cleanup dap-go config

--- lua/plugins/nvim-dap-go.lua
+++ lua/plugins/nvim-dap-go.lua
@@ -5,23 +5,8 @@ return {
   ft = { 'go' },
   dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
   config = function()
-    local status, Job = pcall(require, 'plenary.job')
-    if not status then
-      return
-    end
-
-    local dap
-    status, dap = pcall(require, 'dap')
-    if not status then
-      return
-    end
-    local dapgo
-    status, dapgo = pcall(require, 'dap-go')
-    if not status then
-      return
-    end
-
-    dapgo.setup()
+    local dap_go = require('dap-go')
+    dap_go.setup()
 
     -- copied from dap-go
     local function filtered_pick_process()
@@ -32,51 +17,46 @@ return {
       return require('dap.utils').pick_process(opts)
     end
 
-    local delve_config = {
-      -- time to wait for delve to initialize the debug session.
-      -- default to 20 seconds
-      initialize_timeout_sec = 20,
-      -- a string that defines the port to start delve debugger.
-      -- default to string "${port}" which instructs nvim-dap
-      -- to start the process in a random available port
-      port = '4040',
-    }
-
-    local dap_configs = {
+    -- not using dap-go setup to avoid duplicate entries
+    require('dap').configurations.go = {
       {
         type = 'go',
         name = 'Debug',
         request = 'launch',
         program = '${file}',
-        buildFlags = delve_config.build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
         name = ' Debug (Arguments)',
         request = 'launch',
         program = '${file}',
-        args = function()
-          local args_string = vim.fn.input('Arguments: ')
-          return vim.split(args_string, ' +')
-        end,
-        buildFlags = delve_config.build_flags,
+        args = dap_go.get_arguments,
+        outputMode = 'remote',
+      },
+      {
+        type = 'go',
+        name = ' Debug (Arguments & Build Flags)',
+        request = 'launch',
+        program = '${file}',
+        args = dap_go.get_arguments,
+        buildFlags = dap_go.get_build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
         name = 'Debug Package',
         request = 'launch',
         program = '${fileDirname}',
-        buildFlags = delve_config.build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
         name = ' Debug Package (Arguments)',
         request = 'launch',
         program = '${fileDirname}',
-        args = function()
-          local args_string = vim.fn.input('Arguments: ')
-          return vim.split(args_string, ' +')
-        end,
+        args = dap_go.get_arguments,
+        outputMode = 'remote',
       },
       {
         type = 'go',
@@ -84,7 +64,7 @@ return {
         mode = 'local',
         request = 'attach',
         processId = filtered_pick_process,
-        buildFlags = delve_config.build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
@@ -92,7 +72,7 @@ return {
         request = 'launch',
         mode = 'test',
         program = '${file}',
-        buildFlags = delve_config.build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
@@ -100,56 +80,15 @@ return {
         request = 'launch',
         mode = 'test',
         program = './${relativeFileDirname}',
-        buildFlags = delve_config.build_flags,
+        outputMode = 'remote',
       },
       {
         type = 'go',
         name = ' Attach Remote',
         mode = 'remote',
         request = 'attach',
-      },
-      {
-        type = 'go',
-        name = ' Remote Debug (Boulder)',
-        request = 'attach',
-        mode = 'remote',
-        port = 4040,
-        debugAdapter = 'dlv-dap',
-        host = '127.0.0.1',
-        substitutePath = {
-          {
-            from = '${workspaceFolder}',
-            to = '/boulder',
-          },
-        },
-        before = function()
-          Job:new({
-            command = 'pkill',
-            args = { 'dlv' },
-            on_exit = function(_, return_val)
-              if return_val == 0 then
-                print('found dlv running, killed it (this is a hack)')
-              end
-            end,
-          }):sync() -- or start()
-        end,
+        outputMode = 'remote',
       },
     }
-
-    -- hack is a hack
-    dap.configurations.go = vim.tbl_map(function(c)
-      c.before = function()
-        Job:new({
-          command = 'pkill',
-          args = { 'dlv' },
-          on_exit = function(_, return_val)
-            if return_val == 0 then
-              print('found dlv running, killed it (this is a hack)')
-            end
-          end,
-        }):sync() -- or start()
-      end
-      return c
-    end, dap_configs)
   end,
 }
